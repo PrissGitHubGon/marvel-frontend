ac3b56a5cd03b2164026ff95757264cc07adbb76
priscillia <priscillia.g.pro@outlook.fr>
2022-05-12 18:51:20 +0200

    commit night

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
-import Cookies from "js-cookie";
 import Comics from "./pages/Comics";
 import Characters from "./pages/Characters";
-// import Comicsid from "./pages/Comics-id";
+
 import CharactersId from "./pages/Characters-id";
 
 function App() {
@@ -15,7 +13,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Comics />} />
-          {/* <Route path="/comics/:comicsId" element={<Comicsid />} /> */}
+
           <Route path="/characters" element={<Characters />} />
           <Route path="/character/:characterId" element={<CharactersId />} />
         </Routes>
